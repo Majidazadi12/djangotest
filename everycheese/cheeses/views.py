6e131ad3de5b9d5ff7b1080046f1e063f43cbf51
Majidazadi12 <majidazadi24@gmail.com>
2025-06-16 06:22:30 -0800

    permissions

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -13,7 +13,7 @@ from .models import Cheese
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.mixins import PermissionRequiredMixin
 class HomepageView(TemplateView):
     template_name='index.html'
 class CheeseListView(ListView):
@@ -21,13 +21,15 @@ class CheeseListView(ListView):
 
 class CheeseDetailView(DetailView):
       model=Cheese
-class CheeseCreateView(LoginRequiredMixin,CreateView):
+class CheeseCreateView(PermissionRequiredMixin,LoginRequiredMixin,CreateView):
     model=Cheese
     fields=['name','description','firmness','country_of_origin']
     def form_valid(self, form):
         form.instance.creator=self.request.user
         return super().form_valid(form)
-class CheeseUpdateView(LoginRequiredMixin,UpdateView):
+    permission_required = "cheeses.add_cheese"
+
+class CheeseUpdateView(PermissionRequiredMixin,LoginRequiredMixin,UpdateView):
     model=Cheese
     fields=[
         'name',
@@ -35,4 +37,5 @@ class CheeseUpdateView(LoginRequiredMixin,UpdateView):
         'firmness',
         'country_of_origin'
     ]
+    permission_required = "cheeses.change_cheese"
     action="Update"    
